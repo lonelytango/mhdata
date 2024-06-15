584a7f1fa748af8a1d3d3e4d41632da39d33abf1
Zian Chen <ziandev@gmail.com>
2024-06-15 08:19:38 -0400

    feat: data page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,235 @@
-import Image from "next/image";
+'use client';
+
+import classNames from 'classnames';
+import {
+	type Armor,
+	bodyName,
+	getArmorSuite,
+	getWeaponSuite,
+	type Weapon,
+	type WeaponSuite,
+	type Skill,
+} from '../utils/mhnow_utils';
+
+import { useState } from 'react';
+
+type GearData = {
+	gear: Map<string, Armor>;
+	weapon?: Weapon;
+	allSkills: Skill[];
+};
+
+const initData: GearData = {
+	gear: new Map<string, Armor>(),
+	weapon: undefined,
+	allSkills: [],
+};
+
+const armorSuite = getArmorSuite();
+const weaponSuite = getWeaponSuite();
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [data, setData] = useState<GearData>(initData);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const armorHandler = (item: Armor) => {
+		const { gear, weapon } = data;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		if (gear.get(item.part)?.name === item.name) {
+			gear.delete(item.part);
+		} else {
+			gear.set(item.part, {
+				name: item.name,
+				skills: item.skills,
+				part: item.part,
+			});
+		}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		let rawSkills: Skill[] = [];
+		gear.forEach((value) => {
+			rawSkills = rawSkills.concat(value.skills);
+		});
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		if (weapon) {
+			rawSkills.push({
+				name: weapon.skill ?? '',
+				level: weapon.level ?? 0,
+			});
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+		const skillMap = new Map<string, number>();
+		for (const skill of rawSkills) {
+			if (!skillMap.has(skill.name)) {
+				skillMap.set(skill.name, 0);
+			}
+			skillMap.set(skill.name, skill.level + 1);
+		}
+
+		const skillSet: Skill[] = [];
+		skillMap.forEach((value, key) => {
+			skillSet.push({
+				name: key,
+				level: value,
+			});
+		});
+
+		setData({ gear: gear, allSkills: skillSet });
+	};
+
+	const weaponHandler = (
+		suite: string,
+		weaponName: string,
+		skill: string,
+		level: number
+	) => {
+		let { gear, weapon } = data;
+		const weaponFullName = suite + weaponName;
+		if (weapon?.name === weaponFullName) {
+			weapon = undefined;
+		} else {
+			weapon = {
+				name: weaponFullName,
+				skill: skill,
+				level: level,
+				v: 1,
+			};
+		}
+
+		let rawSkills: Skill[] = [];
+		gear.forEach((value) => {
+			rawSkills = rawSkills.concat(value.skills);
+		});
+
+		if (weapon) {
+			rawSkills.push({
+				name: weapon.skill ?? '',
+				level: weapon.level ?? 0,
+			});
+		}
+
+		const skillMap = new Map<string, number>();
+		for (const skill of rawSkills) {
+			if (!skillMap.has(skill.name)) {
+				skillMap.set(skill.name, 0);
+			}
+			skillMap.set(skill.name, skill.level + 1);
+		}
+
+		const skillSet: Skill[] = [];
+		skillMap.forEach((value, key) => {
+			skillSet.push({
+				name: key,
+				level: value,
+			});
+		});
+
+		setData({ gear, weapon: weapon, allSkills: skillSet });
+	};
+
+	const armorTable = () => {
+		return (
+			<table>
+				<tbody>
+					{armorSuite.map((suite) => (
+						<tr>
+							<td>{suite.suite}</td>
+							{suite.items.map((item) => (
+								<td
+									className={classNames(
+										'w-12 h-12 border text-center cursor-pointer',
+										{
+											'bg-cyan-100': gear.get(item.part)?.name === item.name,
+										}
+									)}
+									onClick={() => armorHandler(item)}
+								>
+									{bodyName(item.part)}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	};
+
+	const weaponTable = () => {
+		return (
+			<table>
+				<tbody>
+					{weaponSuite.map((suite: WeaponSuite) => (
+						<tr>
+							<td>{suite.suite}</td>
+							{suite.weapons.map((item) => {
+								return (
+									<td
+										className={classNames(
+											'w-12 h-12 border text-center cursor-pointer',
+											{
+												'text-gray-100': item.v === 0,
+												'bg-green-100':
+													weapon?.name === suite.suite + item.name,
+											}
+										)}
+										onClick={() =>
+											item.v === 1 &&
+											weaponHandler(
+												suite.suite,
+												item.name,
+												item.skill || suite.skill,
+												item.level || suite.level
+											)
+										}
+									>
+										{item.name}
+									</td>
+								);
+							})}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	};
+
+	const { gear, weapon, allSkills } = data;
+
+	return (
+		<main className='flex flex-row gap-5 h-svh text-xs'>
+			<div className='overflow-y-auto'>
+				{armorTable()}
+				{weaponTable()}
+			</div>
+			<div className='flex flex-row gap-5'>
+				<div>
+					<div>
+						<span>头部: {gear.get('head')?.name}</span>
+					</div>
+					<div>
+						<span>身体: {gear.get('body')?.name}</span>
+					</div>
+					<div>
+						<span>手臂: {gear.get('wrist')?.name}</span>
+					</div>
+					<div>
+						<span>腰部: {gear.get('waist')?.name}</span>
+					</div>
+					<div>
+						<span>腿部: {gear.get('leg')?.name}</span>
+					</div>
+					<div>
+						<span>武器: {weapon?.name}</span>
+					</div>
+				</div>
+				<div className='overflow-y-auto h-64'>
+					{allSkills.map((skill) => {
+						return (
+							<div className='flex flex-row gap-5'>
+								{skill.name} {skill.level}
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</main>
+	);
 }
---
+++ src/utils/mhnow_utils.ts
@@ -0,0 +1,131 @@
+import armorData from '../data/armor.json';
+import skillData from '../data/skills.json';
+import skillGroupData from '../data/skill_group.json';
+import weaponData from '../data/weapon.json';
+
+export interface SkillGroup {
+	type: string;
+	data: SkillDetail[];
+}
+
+export interface SkillDetail {
+	name: string;
+	'max-level': number;
+	type: string;
+	description: string;
+}
+
+export interface GearDetail {
+	name: string;
+	gear: string;
+	level: number;
+}
+
+export interface GearSearchResult {
+	skill: SkillDetail;
+	gears: GearDetail[];
+}
+
+export interface Skill {
+	name: string;
+	level: number;
+}
+
+export interface Armor {
+	name: string;
+	part: string;
+	skills: Skill[];
+}
+
+interface ArmorSuite {
+	suite: string;
+	items: Armor[];
+}
+
+export interface Weapon {
+	name: string;
+	v: number;
+	skill?: string;
+	level?: number;
+}
+
+export interface WeaponSuite {
+	suite: string;
+	weapons: Weapon[];
+	skill: string;
+	level: number;
+	element: string;
+}
+
+export const armorName = (part: string) => {
+	switch (part) {
+		case 'head':
+			return '头盔';
+		case 'body':
+			return '铠甲';
+		case 'wrist':
+			return '腕甲';
+		case 'waist':
+			return '腰甲';
+		case 'leg':
+			return '护腿';
+	}
+};
+
+export const bodyName = (part: string) => {
+	switch (part) {
+		case 'head':
+			return '头';
+		case 'body':
+			return '身';
+		case 'wrist':
+			return '手';
+		case 'waist':
+			return '腰';
+		case 'leg':
+			return '腿';
+	}
+};
+
+export const getSkillGroup = () => {
+	return skillGroupData as SkillGroup[];
+};
+
+export const search = (name: string) => {
+	const data = armorData as ArmorSuite[];
+	const gearArr: GearDetail[] = data.flatMap((armorSet) => {
+		return armorSet.items.flatMap((item) => {
+			return item.skills
+				.filter((skill) => skill.name === name)
+				.flatMap((skillItem) => {
+					return {
+						...skillItem,
+						gear: item.name,
+					};
+				});
+		});
+	});
+
+	const skillDetail = skillData.find((item) => item.name === name);
+
+	const searchResult: GearSearchResult = {
+		skill: skillDetail!,
+		gears: gearArr,
+	};
+	return searchResult;
+};
+
+export const getArmorList = () => {
+	const data = armorData as ArmorSuite[];
+	return data.flatMap((armorSet) => {
+		return armorSet.items.map((item) => item);
+	});
+};
+
+export const getArmorSuite = () => {
+	return armorData;
+};
+
+export const getWeaponSuite = () => {
+	return weaponData;
+};
