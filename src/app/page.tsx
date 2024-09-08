24de983b9c32f47e7d842ac115fee8df3e6ccc24
Zian Chen <zian.chen2@rtx.com>
2024-09-08 11:35:45 -0400

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,236 +3,244 @@
 import classNames from 'classnames';
 import { nanoid } from 'nanoid';
 import {
-	type Armor,
-	bodyName,
-	getArmorSuite,
-	getWeaponSuite,
-	type Weapon,
-	type WeaponSuite,
-	type Skill,
+  type Armor,
+  bodyName,
+  getArmorSuite,
+  getWeaponSuite,
+  type Weapon,
+  type WeaponSuite,
+  type Skill,
 } from '../utils/mhnow_utils';
 import { useState } from 'react';
 
 type GearData = {
-	gear: Map<string, Armor>;
-	weapon?: Weapon;
-	allSkills: Skill[];
+  gear: Map<string, Armor>;
+  weapon?: Weapon;
+  allSkills: Skill[];
 };
 
 const initData: GearData = {
-	gear: new Map<string, Armor>(),
-	weapon: undefined,
-	allSkills: [],
+  gear: new Map<string, Armor>(),
+  weapon: undefined,
+  allSkills: [],
 };
 
 const armorSuite = getArmorSuite();
 const weaponSuite = getWeaponSuite();
 
 const MHCombo = () => {
-	const [data, setData] = useState<GearData>(initData);
+  const [data, setData] = useState<GearData>(initData);
 
-	const armorHandler = (item: Armor) => {
-		const { gear, weapon } = data;
+  const armorHandler = (item: Armor) => {
+    const { gear, weapon } = data;
 
-		if (gear.get(item.part)?.name === item.name) {
-			gear.delete(item.part);
-		} else {
-			gear.set(item.part, {
-				name: item.name,
-				skills: item.skills,
-				part: item.part,
-			});
-		}
+    if (gear.get(item.part)?.name === item.name) {
+      gear.delete(item.part);
+    } else {
+      gear.set(item.part, {
+        name: item.name,
+        skills: item.skills,
+        part: item.part,
+      });
+    }
 
-		let rawSkills: Skill[] = [];
-		gear.forEach((value) => {
-			rawSkills = rawSkills.concat(value.skills);
-		});
+    let rawSkills: Skill[] = [];
+    gear.forEach(value => {
+      rawSkills = rawSkills.concat(value.skills);
+    });
 
-		if (weapon) {
-			rawSkills.push({
-				name: weapon.skill ?? '',
-				level: weapon.level ?? 0,
-			});
-		}
+    if (weapon) {
+      rawSkills.push({
+        name: weapon.skill ?? '',
+        level: weapon.level ?? 0,
+      });
+    }
 
-		const skillMap = new Map<string, number>();
-		for (const skill of rawSkills) {
-			const currentLevel = skillMap.get(skill.name) ?? 0;
-			skillMap.set(skill.name, currentLevel + skill.level);
-		}
+    const skillMap = new Map<string, number>();
+    for (const skill of rawSkills) {
+      const currentLevel = skillMap.get(skill.name) ?? 0;
+      skillMap.set(skill.name, currentLevel + skill.level);
+    }
 
-		const skillSet: Skill[] = [];
-		skillMap.forEach((value, key) => {
-			skillSet.push({
-				name: key,
-				level: value,
-			});
-		});
+    const skillSet: Skill[] = [];
+    skillMap.forEach((value, key) => {
+      skillSet.push({
+        name: key,
+        level: value,
+      });
+    });
 
-		setData({ gear: gear, weapon, allSkills: skillSet });
-	};
+    setData({ gear: gear, weapon, allSkills: skillSet });
+  };
 
-	const weaponHandler = (
-		suite: string,
-		weaponName: string,
-		skill: string,
-		level: number
-	) => {
-		let { gear, weapon } = data;
-		const weaponFullName = suite + weaponName;
-		if (weapon?.name === weaponFullName) {
-			weapon = undefined;
-		} else {
-			weapon = {
-				name: weaponFullName,
-				skill: skill,
-				level: level,
-				v: 1,
-			};
-		}
+  const weaponHandler = (
+    suite: string,
+    weaponName: string,
+    skill: string,
+    level: number,
+    skills?: {
+      name: string;
+      level: number;
+    }[]
+  ) => {
+    let { gear, weapon } = data;
+    const weaponFullName = suite + weaponName;
+    if (weapon?.name === weaponFullName) {
+      weapon = undefined;
+    } else {
+      weapon = {
+        name: weaponFullName,
+        skill: skill,
+        level: level,
+        v: 1,
+        skills,
+      };
+    }
 
-		let rawSkills: Skill[] = [];
-		gear.forEach((value) => {
-			rawSkills = rawSkills.concat(value.skills);
-		});
+    let rawSkills: Skill[] = [];
+    gear.forEach(value => {
+      rawSkills = rawSkills.concat(value.skills);
+    });
 
-		if (weapon) {
-			rawSkills.push({
-				name: weapon.skill ?? '',
-				level: weapon.level ?? 0,
-			});
-		}
+    if (weapon && weapon.skills) {
+      for (const skill of weapon.skills) {
+        rawSkills.push({
+          name: skill.name ?? '',
+          level: skill.level ?? 0,
+        });
+      }
+    }
 
-		const skillMap = new Map<string, number>();
-		for (const skill of rawSkills) {
-			if (!skillMap.has(skill.name)) {
-				skillMap.set(skill.name, 0);
-			}
-			const currentLevel = skillMap.get(skill.name) ?? 0;
-			skillMap.set(skill.name, currentLevel + skill.level);
-		}
+    const skillMap = new Map<string, number>();
+    for (const skill of rawSkills) {
+      if (!skillMap.has(skill.name)) {
+        skillMap.set(skill.name, 0);
+      }
+      const currentLevel = skillMap.get(skill.name) ?? 0;
+      skillMap.set(skill.name, currentLevel + skill.level);
+    }
 
-		const skillSet: Skill[] = [];
-		skillMap.forEach((value, key) => {
-			skillSet.push({
-				name: key,
-				level: value,
-			});
-		});
+    const skillSet: Skill[] = [];
+    skillMap.forEach((value, key) => {
+      skillSet.push({
+        name: key,
+        level: value,
+      });
+    });
 
-		setData({ gear, weapon: weapon, allSkills: skillSet });
-	};
+    setData({ gear, weapon: weapon, allSkills: skillSet });
+  };
 
-	const armorTable = () => {
-		return (
-			<table>
-				<tbody>
-					{armorSuite.map((suite) => (
-						<tr key={nanoid()}>
-							<td>{suite.suite}</td>
-							{suite.items.map((item) => (
-								<td
-									key={nanoid()}
-									className={classNames(
-										'w-12 h-12 border text-center cursor-pointer',
-										{
-											'bg-cyan-100': gear.get(item.part)?.name === item.name,
-										}
-									)}
-									onClick={() => armorHandler(item)}
-								>
-									{bodyName(item.part)}
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		);
-	};
+  const armorTable = () => {
+    return (
+      <table>
+        <tbody>
+          {armorSuite.map(suite => (
+            <tr key={nanoid()}>
+              <td>{suite.suite}</td>
+              {suite.items.map(item => (
+                <td
+                  key={nanoid()}
+                  className={classNames(
+                    'w-12 h-12 border text-center cursor-pointer',
+                    {
+                      'bg-cyan-100': gear.get(item.part)?.name === item.name,
+                    }
+                  )}
+                  onClick={() => armorHandler(item)}
+                >
+                  {bodyName(item.part)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
-	const weaponTable = () => {
-		return (
-			<table>
-				<tbody>
-					{weaponSuite.map((suite: WeaponSuite) => (
-						<tr key={nanoid()}>
-							<td>{suite.suite}</td>
-							{suite.weapons.map((item) => {
-								return (
-									<td
-										key={nanoid()}
-										className={classNames(
-											'w-12 h-12 border text-center cursor-pointer',
-											{
-												'text-gray-100': item.v === 0,
-												'bg-green-100':
-													weapon?.name === suite.suite + item.name,
-											}
-										)}
-										onClick={() =>
-											item.v === 1 &&
-											weaponHandler(
-												suite.suite,
-												item.name,
-												item.skill || suite.skill,
-												item.level || suite.level
-											)
-										}
-									>
-										{item.name}
-									</td>
-								);
-							})}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		);
-	};
+  const weaponTable = () => {
+    return (
+      <table>
+        <tbody>
+          {weaponSuite.map((suite: WeaponSuite) => (
+            <tr key={nanoid()}>
+              <td>{suite.suite}</td>
+              {suite.weapons.map(item => {
+                return (
+                  <td
+                    key={nanoid()}
+                    className={classNames(
+                      'w-12 h-12 border text-center cursor-pointer',
+                      {
+                        'text-gray-100': item.v === 0,
+                        'bg-green-100':
+                          weapon?.name === suite.suite + item.name,
+                      }
+                    )}
+                    onClick={() =>
+                      item.v === 1 &&
+                      weaponHandler(
+                        suite.suite,
+                        item.name,
+                        item.skill || suite.skill,
+                        item.level || suite.level,
+                        suite.skills
+                      )
+                    }
+                  >
+                    {item.name}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
-	const { gear, weapon, allSkills } = data;
+  const { gear, weapon, allSkills } = data;
 
-	return (
-		<main className='flex flex-row gap-5 h-svh text-xs p-4'>
-			<div className='overflow-y-auto'>
-				{armorTable()}
-				{weaponTable()}
-			</div>
-			<div className='flex flex-row gap-5'>
-				<div>
-					<div>
-						<span>头部: {gear.get('head')?.name}</span>
-					</div>
-					<div>
-						<span>身体: {gear.get('body')?.name}</span>
-					</div>
-					<div>
-						<span>手臂: {gear.get('wrist')?.name}</span>
-					</div>
-					<div>
-						<span>腰部: {gear.get('waist')?.name}</span>
-					</div>
-					<div>
-						<span>腿部: {gear.get('leg')?.name}</span>
-					</div>
-					<div>
-						<span>武器: {weapon?.name}</span>
-					</div>
-				</div>
-				<div className='overflow-y-auto h-64'>
-					{allSkills.map((skill) => {
-						return (
-							<div key={nanoid()} className='flex flex-row gap-5'>
-								{skill.name} {skill.level}
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex flex-row gap-5 h-svh text-xs p-4">
+      <div className="overflow-y-auto">
+        {armorTable()}
+        {weaponTable()}
+      </div>
+      <div className="flex flex-row gap-5">
+        <div>
+          <div>
+            <span>头部: {gear.get('head')?.name}</span>
+          </div>
+          <div>
+            <span>身体: {gear.get('body')?.name}</span>
+          </div>
+          <div>
+            <span>手臂: {gear.get('wrist')?.name}</span>
+          </div>
+          <div>
+            <span>腰部: {gear.get('waist')?.name}</span>
+          </div>
+          <div>
+            <span>腿部: {gear.get('leg')?.name}</span>
+          </div>
+          <div>
+            <span>武器: {weapon?.name}</span>
+          </div>
+        </div>
+        <div className="overflow-y-auto h-64">
+          {allSkills.map(skill => {
+            return (
+              <div key={nanoid()} className="flex flex-row gap-5">
+                {skill.name} {skill.level}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MHCombo;
--- src/utils/mhnow_utils.ts
+++ src/utils/mhnow_utils.ts
@@ -4,128 +4,130 @@ import skillGroupData from '../data/skill_group.json';
 import weaponData from '../data/weapon.json';
 
 export interface SkillGroup {
-	type: string;
-	data: SkillDetail[];
+  type: string;
+  data: SkillDetail[];
 }
 
 export interface SkillDetail {
-	name: string;
-	'max-level': number;
-	type: string;
-	description: string;
+  name: string;
+  'max-level': number;
+  type: string;
+  description: string;
 }
 
 export interface GearDetail {
-	name: string;
-	gear: string;
-	level: number;
+  name: string;
+  gear: string;
+  level: number;
 }
 
 export interface GearSearchResult {
-	skill: SkillDetail;
-	gears: GearDetail[];
+  skill: SkillDetail;
+  gears: GearDetail[];
 }
 
 export interface Skill {
-	name: string;
-	level: number;
+  name: string;
+  level: number;
 }
 
 export interface Armor {
-	name: string;
-	part: string;
-	skills: Skill[];
+  name: string;
+  part: string;
+  skills: Skill[];
 }
 
 interface ArmorSuite {
-	suite: string;
-	items: Armor[];
+  suite: string;
+  items: Armor[];
 }
 
 export interface Weapon {
-	name: string;
-	v: number;
-	skill?: string;
-	level?: number;
+  name: string;
+  v: number;
+  skill?: string;
+  level?: number;
+  skills?: Skill[];
 }
 
 export interface WeaponSuite {
-	suite: string;
-	weapons: Weapon[];
-	skill: string;
-	level: number;
-	element: string;
+  suite: string;
+  weapons: Weapon[];
+  skill: string;
+  level: number;
+  element: string;
+  skills?: Skill[];
 }
 
 export const armorName = (part: string) => {
-	switch (part) {
-		case 'head':
-			return '头盔';
-		case 'body':
-			return '铠甲';
-		case 'wrist':
-			return '腕甲';
-		case 'waist':
-			return '腰甲';
-		case 'leg':
-			return '护腿';
-	}
+  switch (part) {
+    case 'head':
+      return '头盔';
+    case 'body':
+      return '铠甲';
+    case 'wrist':
+      return '腕甲';
+    case 'waist':
+      return '腰甲';
+    case 'leg':
+      return '护腿';
+  }
 };
 
 export const bodyName = (part: string) => {
-	switch (part) {
-		case 'head':
-			return '头';
-		case 'body':
-			return '身';
-		case 'wrist':
-			return '手';
-		case 'waist':
-			return '腰';
-		case 'leg':
-			return '腿';
-	}
+  switch (part) {
+    case 'head':
+      return '头';
+    case 'body':
+      return '身';
+    case 'wrist':
+      return '手';
+    case 'waist':
+      return '腰';
+    case 'leg':
+      return '腿';
+  }
 };
 
 export const getSkillGroup = () => {
-	return skillGroupData as SkillGroup[];
+  return skillGroupData as SkillGroup[];
 };
 
 export const search = (name: string) => {
-	const data = armorData as ArmorSuite[];
-	const gearArr: GearDetail[] = data.flatMap((armorSet) => {
-		return armorSet.items.flatMap((item) => {
-			return item.skills
-				.filter((skill) => skill.name === name)
-				.flatMap((skillItem) => {
-					return {
-						...skillItem,
-						gear: item.name,
-					};
-				});
-		});
-	});
+  const data = armorData as ArmorSuite[];
+  const gearArr: GearDetail[] = data.flatMap(armorSet => {
+    return armorSet.items.flatMap(item => {
+      return item.skills
+        .filter(skill => skill.name === name)
+        .flatMap(skillItem => {
+          return {
+            ...skillItem,
+            gear: item.name,
+          };
+        });
+    });
+  });
 
-	const skillDetail = skillData.find((item) => item.name === name);
+  const skillDetail = skillData.find(item => item.name === name);
 
-	const searchResult: GearSearchResult = {
-		skill: skillDetail!,
-		gears: gearArr,
-	};
-	return searchResult;
+  const searchResult: GearSearchResult = {
+    skill: skillDetail!,
+    gears: gearArr,
+  };
+  return searchResult;
 };
 
 export const getArmorList = () => {
-	const data = armorData as ArmorSuite[];
-	return data.flatMap((armorSet) => {
-		return armorSet.items.map((item) => item);
-	});
+  const data = armorData as ArmorSuite[];
+  return data.flatMap(armorSet => {
+    return armorSet.items.map(item => item);
+  });
 };
 
 export const getArmorSuite = () => {
-	return armorData;
+  return armorData;
 };
 
 export const getWeaponSuite = () => {
-	return weaponData;
+  return weaponData;
 };
